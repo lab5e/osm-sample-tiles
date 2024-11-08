def53d60eceab6b1c2132f77e7fb7f6ebbae5017
Ståle Dahl <stalehd@gmail.com>
2024-11-08 10:06:12 +0100

    Return fs.FS

--- sample-tiles.go
+++ sample-tiles.go
@@ -1,12 +1,15 @@
 package osmsampletiles
 
-import "embed"
+import (
+	"embed"
+	"io/fs"
+)
 
 //go:embed map/tiles
 var tiles embed.FS
 
 // SampleOSMTiles returns an embeddded file system with Open Street Map tiles containting sample data. The
 // area is roughly the city center of Trondheim.
-func SampleOSMTiles() embed.FS {
+func SampleOSMTiles() fs.FS {
 	return tiles
 }
